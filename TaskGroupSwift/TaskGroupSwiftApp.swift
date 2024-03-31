13a8690f0ebbea44e939172d9e551e74d8291dda
Ayush Gupta <dev.iayush27@gmail.com>
2024-04-01 01:25:28 +0530

    Initial Commit

---
+++ TaskGroupSwift/TaskGroupSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskGroupSwiftApp.swift
+//  TaskGroupSwift
+//
+//  Created by Ayush Gupta on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskGroupSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
